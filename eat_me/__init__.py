9291412f2e37bea180fa9fdd9cbf94f6b2957855
Taras Drapalyuk <taras@drapalyuk.com>
2015-11-02 15:40:27 +0300

    init

---
+++ eat_me/__init__.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+from functools import partial
+
+from plumbum import local, colors, cli
+from plumbum.commands.processes import ProcessExecutionError
+
+__author__ = 'Taras Drapalyuk <taras@drapalyuk.com>'
+__date__ = '02.11.2015'
+__version__ = '0.0.1'
+
+
+def get_repos(start_path='.'):
+    for file_name in os.listdir(start_path) + [start_path]:
+        if os.path.isdir(file_name) and not file_name.startswith('..'):
+            if os.path.exists(os.path.join(file_name, '.hg')):
+                yield file_name
+
+
+def run_for_all_repos(func, start_path='.'):
+    for repo_path in get_repos(start_path):
+        func(path=repo_path)
+
+
+def pull_update(path, branch=None, clean=False):
+    """hg pull + hg update"""
+    hg = local['hg']
+    hg = hg['--repository', path]
+    hg_pull = hg['pull']
+    hg_update = hg['update']
+
+    if clean is True:
+        hg_update = hg_update['--clean']
+
+    if branch:
+        hg_update = hg_update['--rev', branch]
+
+    with colors.green:
+        print(path)
+
+    with colors.yellow:
+        print(hg_pull)
+
+    print(hg_pull())
+
+    with colors.yellow:
+        print(hg_update)
+
+    # Игнорируем ошибки:
+    # 255 - unknown revision
+    try:
+        print(hg_update(retcode=[0]))
+    except ProcessExecutionError as e:
+        with colors.red:
+            print(e.stderr)
+
+
+def push(path, branch=None, new_branch=True):
+    """hg push"""
+    hg = local['hg']
+    hg = hg['--repository', path]
+
+    hg_push = hg['push']
+
+    if branch:
+        hg_push = hg_push['--branch', branch]
+
+    if new_branch is True:
+        hg_push = hg_push['--new-branch']
+
+    with colors.green:
+        print(path)
+
+    with colors.yellow:
+        print(hg_push)
+
+    print(hg_push())
+
+
+def status(path, branch=None, new_branch=True):
+    """hg status"""
+    hg = local['hg']
+    hg = hg['--repository', path]
+
+    hg_status = hg['status']
+
+    with colors.green:
+        print(path)
+
+    with colors.yellow:
+        print(hg_status)
+
+    print(hg_status())
+
+
+class EatMe(cli.Application):
+    PROGNAME = 'eat_me'
+    VERSION = __version__
+    verbose = cli.Flag(["v", "verbose"])
+
+    _branch = None
+    _clean = False
+
+    def main(self, *args):
+        if args:
+            print "Unknown command %r" % (args[0],)
+            return 1  # error exit code
+        if not self.nested_command:  # will be ``None`` if no sub-command follows
+            print "No command given"
+            return 1  # error exit code
+
+
+@EatMe.subcommand("push")
+class Push(cli.Application):
+    _clean = False
+    _branch = None
+
+    @cli.switch(["-C", "--clean"])
+    def allow_clean(self):
+        """hg update --clean"""
+        self._clean = True
+
+    @cli.switch(["-b", "--branch"])
+    def set_branch(self, branch):
+        """hg update --rev BRANCH"""
+        self._branch = branch
+
+    def main(self):
+        hg_push = partial(push, branch=self._branch, clean=self._clean)
+        run_for_all_repos(hg_push)
+
+
+@EatMe.subcommand("update")
+class Update(cli.Application):
+    _clean = False
+    _branch = None
+
+    @cli.switch(["-C", "--clean"])
+    def allow_clean(self):
+        """hg update --clean"""
+        self._clean = True
+
+    @cli.switch(["-b", "--branch"])
+    def set_branch(self, branch):
+        """hg update --rev BRANCH"""
+        self._branch = branch
+
+    def main(self):
+        hg_pull_update = partial(pull_update, branch=self._branch, clean=self._clean)
+        run_for_all_repos(hg_pull_update)
+
+
+@EatMe.subcommand("status")
+class Status(cli.Application):
+    def main(self):
+        run_for_all_repos(status)
+
+
+def run_cli():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--branch', help=u'branch name')
+    parser.add_argument('-C', '--clean', action='store_true', default=False,
+                        help=u'discard local changes (no backup). Can only be used with branch option (-b)')
+    parser.add_argument('--push', action='store_true', default=False,
+                        help=u'hg push')
+    parser.add_argument('--update', action='store_true', default=False,
+                        help=u'hg pull + hg update')
+
+    parser.add_argument('--status', action='store_true', default=False,
+                        help=u'hg status')
+
+    args = parser.parse_args()
+
+    if args.update:
+        hg_pull_update = partial(pull_update, branch=args.branch, clean=args.clean)
+        run_for_all_repos(hg_pull_update)
+
+    if args.push:
+        hg_push = partial(push, branch=args.branch, clean=args.clean)
+        run_for_all_repos(hg_push)
+
+    if args.status:
+        run_for_all_repos(status)
+
+
+if __name__ == '__main__':
+    EatMe.run()
+    # run_cli()
---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import os
+
+with open('eat_me/__init__.py', 'r') as fd:
+    PACKAGE_VERSION = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                                fd.read(), re.MULTILINE).group(1)
+
+if not PACKAGE_VERSION:
+    raise RuntimeError('Cannot find version information')
+
+this_dir = os.path.dirname(__file__)
+readme_filename = os.path.join(this_dir, 'README.md')
+requirements_filename = os.path.join(this_dir, 'requirements.txt')
+
+with open(readme_filename) as f:
+    PACKAGE_LONG_DESCRIPTION = f.read()
+
+with open(requirements_filename) as f:
+    # Игнорируем комментарии,
+    # пустые строки и пакеты из репозиториев (-e)
+    PACKAGE_INSTALL_REQUIRES = [
+        line.strip('\n') for line in f
+        if not line.startswith('#') and not line.startswith('-e') and line.strip()
+        ]
+
+setup(
+    name='EatMe',
+    version=PACKAGE_VERSION,
+    author='Taras Drapalyuk',
+    author_email='taras@drapalyuk.com',
+    description='HiC EatMe Utility',
+    url='https://hiconversion.ru/',
+    # long_description=PACKAGE_LONG_DESCRIPTION,
+    packages=[
+        'eat_me',
+    ],
+    entry_points={
+        'console_scripts': [
+            'eat_me = eat_me:EatMe.run',
+        ],
+    },
+    install_requires=PACKAGE_INSTALL_REQUIRES,
+)
