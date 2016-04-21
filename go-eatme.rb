7a4b554315554d6d3273eee05bcc4e493887f0d6
Taras Drapalyuk <taras@drapalyuk.com>
2016-04-21 10:33:19 +0300

    upd brew formula

--- go-eatme.rb
+++ go-eatme.rb
@@ -1,9 +1,9 @@
 class GoEatme < Formula
   desc ""
   homepage ""
-  url "https://github.com/kulapard/go-eatme/archive/0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "9e1d32b54ff0134f61505312541c1a7e8b3b2d55278aa1847071f27cd3c9f9f5"
+  url "https://github.com/kulapard/go-eatme/archive/0.1.1.tar.gz"
+  version "0.1.1"
+  sha256 "bf56b17606921ca2cf483b87385cbca98721ee0322d32a0a621b24d0adb55252"
 
   depends_on "go" => :build
 
