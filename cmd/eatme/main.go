ec3b8fb32775be35ae3ffac1f06b671583544788
Taras Drapalyuk <taras@drapalyuk.com>
2023-12-09 21:01:35 +0400

    refactor package structure

--- cmd/eatme/main.go
+++ cmd/eatme/main.go
@@ -1,13 +1,9 @@
 package main
 
-import (
-	"github.com/kulapard/go-eatme/internal/cli"
-)
-
 var (
 	version = "unknown-local-build"
 )
 
 func main() {
-	cli.Execute(version)
+	Execute(version)
 }
