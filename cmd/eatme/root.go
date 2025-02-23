4c190230cae40d661c51339c9eaef44b6973567e
Taras Drapalyuk <taras@drapalyuk.com>
2025-02-23 15:27:56 +0100

    fix goreleaser (#24)

--- cmd/eatme/root.go
+++ cmd/eatme/root.go
@@ -9,25 +9,29 @@ import (
 	"github.com/kulapard/go-eatme/internal/runner"
 )
 
-var branch string
-var all bool
+type config struct {
+	branch string
+	all    bool
+}
+
+var cfg = &config{}
 
 var rootCmd = &cobra.Command{
 	Use:   "eatme",
 	Short: "pull + update",
 	Run: func(cmd *cobra.Command, args []string) {
-		c := runner.CliCommand{Name: "pull + update", Branch: branch}
+		c := runner.CliCommand{Name: "pull + update", Branch: cfg.branch}
 		c.RunRecursively()
 	},
 	CompletionOptions: cobra.CompletionOptions{DisableDefaultCmd: true},
 }
 
 func Execute(version string) {
-	rootCmd.PersistentFlags().StringVarP(&branch, "branch", "b", "", "Branch or Tag name")
-	cmdPush.PersistentFlags().BoolVarP(&all, "all", "a", false, "Push all branches")
+	rootCmd.PersistentFlags().StringVarP(&cfg.branch, "branch", "b", "", "Branch or Tag name")
+	cmdPush.PersistentFlags().BoolVarP(&cfg.all, "all", "a", false, "Push all branches")
 	rootCmd.Version = version
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing CLI '%s'", err)
+		fmt.Fprintf(os.Stderr, "Error executing command: %v\n", err)
 		os.Exit(1)
 	}
 }
--- cmd/eatme/update.go
+++ cmd/eatme/update.go
@@ -9,7 +9,7 @@ var cmdUpdate = &cobra.Command{
 	Use:   "update",
 	Short: "run git checkout/hg update",
 	Run: func(cmd *cobra.Command, args []string) {
-		c := runner.CliCommand{Name: "update", Branch: branch}
+		c := runner.CliCommand{Name: "update", Branch: cfg.branch}
 		c.RunRecursively()
 	},
 }
